c0e30dbf54b92485f0648b4513d0bd149167fa83
sya-ri <git@s7a.dev>
2023-11-13 12:45:21 +0900

    chore: Setup ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
+
+val ktlint: Configuration by configurations.creating
 
 dependencies {
     testImplementation(kotlin("test"))
+    ktlint("com.pinterest.ktlint:ktlint-cli:1.1.0-kotlin-dev-SNAPSHOT") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
 }
 
 tasks.test {
@@ -24,4 +32,21 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+val ktlintCheck by tasks.registering(JavaExec::class) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+    args(
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+    )
+}
+
+tasks.check {
+    dependsOn(ktlintCheck)
 }
